bd5ee96e4e7b567f6c34f05ce7b80d198bad1f0b
DANIEL <DANIEL@192.168.100.16>
2015-07-07 12:27:38 -0300

    Avaliação 3;
    Primeira tentativa

--- src/main/java/Avaliacao3.java
+++ src/main/java/Avaliacao3.java
@@ -3,9 +3,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import static java.lang.System.in;
 import static java.lang.System.out;
+import java.util.ArrayList;
 import java.util.List;
 import utfpr.ct.dainf.if62c.avaliacao.Lancamento;
 import java.util.Scanner;
+import utfpr.ct.dainf.if62c.avaliacao.ProcessaLancamentos;
 
 /**
  * IF62C Fundamentos de Programação 2 Avaliação parcial.
@@ -16,9 +18,14 @@ public class Avaliacao3 {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
+        ArrayList<Lancamento> lista1 = new ArrayList<>();
+        String caminho;
         Scanner scanner = new Scanner(in);
         out.print("Digite o caminho do arquivo de lançamentos: ");
-
+        caminho = scanner.next();
+        
+        ProcessaLancamentos processor = new ProcessaLancamentos(caminho);
+        lista1 = processor.getLancamentos();
         
     }
 
